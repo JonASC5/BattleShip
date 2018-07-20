b1f8d38388802221607164d407579c26217f0732
JonASC5 <jonjonf1@icloud.com>
2018-07-20 08:29:53 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,92 @@
+function setup(){
+    createCanvas(1000, 1000);
+    fill("grey");
+    let i=0;
+    while(i < 1100){
+        rect(100, i, 100, 100);
+        rect(200, i, 100, 100);
+        rect(300, i, 100, 100);
+        rect(400, i, 100, 100);
+        rect(500, i, 100, 100);
+        rect(600, i, 100, 100);
+        rect(700, i, 100, 100);
+        rect(800, i, 100, 100);
+        rect(900, i, 100, 100);
+        rect(1000, i, 100, 100);
+        i+=100;
+        stroke("blue");
+        rect((Math.floor(random()*10)*100), (Math.floor(random(2, 9))* 100), 100, 300);
+
+        stroke("blue");
+        rect((Math.floor(random()*10)*100), (Math.floor(random(2, 9))* 100), 100, 300);
+ }
+}
+
+    let j = 100
+    let k = 200
+    let f = 600
+    let d = 700
+    let x = 500
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function setup(){
+      //  draw("rect");
+   // }
+
+   /* fill("grey");
+    rect(50, 50, 50, 50);
+    fill("grey");
+    rect(100, 50, 100, 50);
+    fill("grey");
+    rect(150, 50, 100, 50);
+    fill("grey");
+    rect(550, 67, 100, 50);
+    fill("grey");
+    rect(550, 115, 100, 50);
+    fill("grey");
+    rect(550, 160, 100, 50);
+    fill("grey");
+    rect(32, 300, 100, 50);
+    fill("grey");
+    rect(32, 350, 100, 50);
+    fill("grey");
+    rect(32, 400, 100, 50);
+    fill("grey");
+    rect(720, 550, 100, 50);
+    fill("grey");
+    rect(770, 550, 100, 50);
+    fill("grey");*/
+
+
+
